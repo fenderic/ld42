1d7844e43b9a3131cf11aadfbd55e3acb561624b
Jacob Howcroft <jacob.howcroft@gmail.com>
2018-08-11 11:25:12 -0700

    Add boilerplate stuff for GCP

---
+++ server/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+io.on('connection', function(client){
+  client.on('event', function(data){});
+  client.on('disconnect', function(){});
+});
+server.listen(config.get('PORT'));
+
+module.exports = server;
